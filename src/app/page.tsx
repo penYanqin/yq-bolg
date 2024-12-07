fdaa2e7b56231085d4189797dd55f120b0c089b3
yq <3390836086@qq.com>
2024-12-07 16:18:28 +0800

    mongoDb配置过时修改

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@
  * @Author: pyq
  * @Date: 2024-11-29 15:03:43
  * @LastEditors: pyq
- * @LastEditTime: 2024-12-07 11:21:02
+ * @LastEditTime: 2024-12-07 16:14:42
  */
+
 export default function Home() {
   return <div className="home text-red-500">home</div>;
 }
--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: pyq
+ * @Date: 2024-12-07 13:58:02
+ * @LastEditors: pyq
+ * @LastEditTime: 2024-12-07 16:18:18
+ */
 import mongoose from "mongoose";
 
 let initialized = false;
@@ -9,9 +17,7 @@ export async function connect() {
 
   try {
     // 连接到 MongoDB，只传入必要的配置
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "yq-blog", // 指定数据库名
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("Connected to MongoDB");
     initialized = true; // 确保连接只初始化一次
