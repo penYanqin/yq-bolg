c6bc09c3d2ca82bdfaafb656c952bf59c1346afb
yq <3390836086@qq.com>
2024-12-12 17:08:04 +0800

    打包bug

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,10 @@
  * @Author: pyq
  * @Date: 2024-11-29 15:03:43
  * @LastEditors: pyq
- * @LastEditTime: 2024-12-12 16:42:45
+ * @LastEditTime: 2024-12-12 17:08:01
  */
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true, // 忽略 TypeScript 错误，不让它影响构建
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
