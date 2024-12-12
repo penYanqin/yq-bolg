0f82cb9f852e94b3b64260d0be3dea81994eb613
yq <3390836086@qq.com>
2024-12-12 17:05:05 +0800

    打包bug

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,13 @@
  * @Author: pyq
  * @Date: 2024-11-29 15:03:43
  * @LastEditors: pyq
- * @LastEditTime: 2024-12-12 16:36:10
+ * @LastEditTime: 2024-12-12 16:42:45
  */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // 忽略 TypeScript 错误，不让它影响构建
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // 忽略 ESLint 错误
   },
 };
 
