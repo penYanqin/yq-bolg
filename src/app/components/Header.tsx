a3a06349c108a2ebd27764e313775b3d1cc457ad
yq <3390836086@qq.com>
2024-12-12 14:55:18 +0800

    添价仪表盘功能（完成）

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -37,7 +37,7 @@ const Header: FC<IProps> = () => {
           className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
         >
           <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-            Sahand&apos;
+            pyq&apos;
           </span>
           Blog
         </Link>
